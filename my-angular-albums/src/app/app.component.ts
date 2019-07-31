c33c861320c6b252a4e1eb4a24449ffb06cda753
cprenegade <cprenegade@yahoo.com>
2019-07-31 10:20:02 -0400

    Wednesday mid morning

--- my-angular-albums/src/app/app.component.ts
+++ my-angular-albums/src/app/app.component.ts
@@ -1,67 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { Album } from "./albums/album.model";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-// tslint:disable-next-line: one-line
-export class AppComponent implements OnInit{
-    title = 'Project Angular(07/2019): ';
-  name = "Chuck's albums";
-  albumsArray: Album[];
-  titleCounter = 1;
- ngOnInit(): void {
-  const interval = setInterval(() => this.titleCounter++, 2000);
-  setTimeout(() => clearInterval(interval), 6000);
-  this.albumsArray = [
-      {
-        id: 1,
-        artist: "Tremonti",
-        albumName: "Dust",
-        isOnSale: true,
-        price: 11.99,
-        currency: "USD",
-        year: 2016,
-        releaseDate: "April 29, 2016",
-        recordingLocation: "Studio Barbarosa, Orlando, FL",
-        genre: "Pop/Rock",
-        duration: "43:18:00",
-        url: "https://www.allmusic.com/album/dust-mw0002918360"
-      },
-      {
-        id: 2,
-        artist: "Bon Jovi",
-        albumName: "7800 Fahrerenheit",
-        isOnSale: false,
-        price: 7,
-        year: 1985,
-        currency: "USD",
-        releaseDate: "April 20, 1985",
-        recordingLocation: "Warehouse, Philadelphia, PA",
-        genre: "Pop/Rock",
-        duration: "47:15:00",
-        url: "https://www.allmusic.com/album/7800%C2%B0-fahrenheit-mw0000189199"
-      },
-      {
-        id: 3,
-        artist: "The Beatles",
-        albumName: "The White Album",
-        isOnSale: true,
-        currency: "GBP",
-        price: 24,
-        year: 1968,
-        releaseDate: "November 22, 1968",
-        recordingLocation: "",
-        genre: "Pop/Rock",
-        duration: "1:33:43",
-        url: "https://www.allmusic.com/album/the-beatles-white-album-mw0000418113"
-
-        // tslint:disable-next-line: no-trailing-whitespace
-        
-      }];
-    console.log(this.albumsArray);
-  }
+export class AppComponent {
+  title = "Project Angular:  Chuck's albums";
 }
 
