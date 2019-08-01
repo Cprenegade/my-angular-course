0b22456ec2b0081fbe88b333803d084fcc6d0225
cprenegade <cprenegade@yahoo.com>
2019-08-01 12:37:42 -0400

    Ch 8 services working

--- my-angular-albums/src/app/albums/shared/album.service.ts
+++ my-angular-albums/src/app/albums/shared/album.service.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Album } from '../album.model';
 
-import { ALBUMS } from "../albums.data";
 
 @Injectable({
   providedIn: "root"
